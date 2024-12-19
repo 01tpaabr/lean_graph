aa18acb13c6bf5dc924e6c288f5dffbadf97224d
Thiago Andrade <01tpaa.br@gmail.com>
2024-12-19 02:07:35 -0300

    concept of neighbors

--- LeanGraph.lean
+++ LeanGraph.lean
@@ -41,17 +41,42 @@ def listWrapperEdgeIsEqual (e1:edge) (e2:edge) : List edge :=
   if edgeIsEqual e1 e2 then [e1] else []
 
 -- Wrapper to handle termination problem with g.edges
+-- 'f' function to return value for 'h' (element of Edge List), using arbitrary α as argument
+-- 'composeFunction' to compose results from 'f' for each step
 def applyFunctionInDecreasingEdgeList {α : Type} {β : Type} (baseValue : β) (edges: List edge)
 (composeFunction: β → β → β) (f: edge → α → β) (auxArg: α) : β :=
   match edges with
   | [] => baseValue
   | h :: t => composeFunction (f h auxArg) (applyFunctionInDecreasingEdgeList baseValue t composeFunction f auxArg)
 
+-- No other edges equal to this one in the graph
 def edgeIsUniqueInGraph (e: edge) (g: graph) : Bool :=
   applyFunctionInDecreasingEdgeList (true) (g.edges) (andFunction) (edgeIsNotEqual) (e)
 
 def repeatedEdgesInGraph (e: edge) (g: graph) : List edge :=
   applyFunctionInDecreasingEdgeList ([]) (g.edges) (aggregEdges) (listWrapperEdgeIsEqual) (e)
+
+def edgeOccursOnlyOnceInGraph (e: edge) (g: graph) : Bool := if (repeatedEdgesInGraph e g).length = 1 then true else false
+
+-- No repetition of Edges
+def isProperGraph (g: graph) : Bool :=
+  applyFunctionInDecreasingEdgeList (true) (g.edges) (andFunction) (edgeOccursOnlyOnceInGraph) (g)
+
+def edgeFallsUponNode (e: edge) (n: node) : Bool := if nodeIsEqual n e.a ∨ nodeIsEqual n e.b then true else false
+
+-- Just to apply to polymorphic function as α
+def listWrapperEdgeFallsUponNode (e: edge) (n: node) : List edge := if nodeIsEqual n e.a ∨ nodeIsEqual n e.b then [e] else []
+
+def edgesThatFallsUponNode (g : graph) (n : node) : List edge :=
+  applyFunctionInDecreasingEdgeList ([]) (g.edges) (aggregEdges) (listWrapperEdgeFallsUponNode) (n)
+
+def edgeBetweenNodes (e: edge) (n1: node) (n2: node) : Bool := if (nodeIsEqual e.a n1 ∧ nodeIsEqual e.b n2) ∨ (nodeIsEqual e.a n2 ∧ nodeIsEqual e.b n1) then true else false
+
+-- Just to apply to polymorphic function as α
+def tupleWrapperEdgeBetweenNodes (e: edge) (nodeTuple : (node × node)) : Bool := edgeBetweenNodes e (nodeTuple.1) (nodeTuple.2)
+
+def areNeighbors (g : graph) (n1: node) (n2: node) :=
+  applyFunctionInDecreasingEdgeList (false) (g.edges) (orFunction) (tupleWrapperEdgeBetweenNodes) ((n1, n2))
 
 
 def nullName : Int := 0
@@ -63,8 +88,11 @@ def testNode3 : node := {name:= 3}
 
 def edge_1 : edge := {a:= testNode, b:= testNode2}
 def edge_2 : edge := {a:=testNode2, b:=testNode3}
+def edge_3 : edge := {a:=testNode3, b:=testNode}
 
 def nullGraph : graph := {edges:=[]}
 def example_graph : graph := {edges:=[edge_1, edge_2]}
 
-#eval repeatedEdgesInGraph edge_1 example_graph
+#eval isProperGraph example_graph
+#eval edgesThatFallsUponNode example_graph testNode3
+#eval areNeighbors example_graph testNode3 testNode2
